e00324c7f5a128742239fe865cc270800f2e6e99
Tobias Bieniek <tobias.bieniek@gmail.com>
2017-05-02 17:53:43 +0200

    auto-dist-tag: Add "write" option

--- src/auto-dist-tag.js
+++ src/auto-dist-tag.js
@@ -3,13 +3,23 @@ const pkgUp = require('pkg-up');
 const getDistTags = require('./fetch-dist-tags');
 const calcDistTag = require('./calc-dist-tag');
 
-module.exports = function autoDistTag(cwd) {
-  let pkgPath, pkg;
+module.exports = function autoDistTag(cwd, options) {
+  let pkgPath, pkg, tags, tag;
 
   return pkgUp(cwd)
     .then(_pkgPath => (pkgPath = _pkgPath))
     .then(() => fs.readJson(pkgPath))
     .then(_pkg => (pkg = _pkg))
     .then(() => getDistTags(pkg.name))
-    .then(distTags => calcDistTag(pkg.version, distTags));
+    .then(_tags => (tags = _tags))
+    .then(() => calcDistTag(pkg.version, tags))
+    .then(_tag => (tag = _tag))
+    .then(() => {
+      if (options && options.write) {
+        pkg.publishConfig = pkg.publishConfig || {};
+        pkg.publishConfig.tag = pkg.publishConfig.tag || tag;
+      }
+      return fs.writeJson(pkgPath, pkg, { spaces: 2 });
+    })
+    .then(() => tag);
 };
