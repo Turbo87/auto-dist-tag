0f4ba19f2e880449350020515974069eb3f1bcc6
Tobias Bieniek <tobias.bieniek@gmail.com>
2017-05-04 14:29:17 +0200

    fetch-dist-tags: Return empty object if package does not exist yet

--- src/fetch-dist-tags.js
+++ src/fetch-dist-tags.js
@@ -4,5 +4,12 @@ const packageJson = require('package-json');
 
 module.exports = function fetchDistTags(packageName) {
   return packageJson(packageName, { allVersions: true })
-    .then(json => json['dist-tags']);
+    .then(json => json['dist-tags'])
+    .catch(error => {
+      if (error && error.message === `Package \`${packageName}\` doesn't exist`) {
+        return {};
+      }
+
+      throw error;
+    });
 };
