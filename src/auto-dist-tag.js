d0bbadd85d04d096dac9d4582ced45b966b15584
Tobias Bieniek <tobias.bieniek@gmail.com>
2017-10-27 11:22:22 +0200

    Only write to "package.json" file if necessary

--- src/auto-dist-tag.js
+++ src/auto-dist-tag.js
@@ -18,10 +18,21 @@ module.exports = function autoDistTag(cwd, options) {
     .then(_tag => (tag = _tag))
     .then(() => {
       if (options && options.write) {
+        // skip writing to `package.json if an explicit publishConfig.tag is set
+        if ('publishConfig' in pkg && 'tag' in pkg.publishConfig) {
+          return;
+        }
+
+        // skip writing to `package.json if the calculated tag is "latest" because it's the default anyway
+        if (tag === 'latest') {
+          return;
+        }
+
         pkg.publishConfig = pkg.publishConfig || {};
-        pkg.publishConfig.tag = pkg.publishConfig.tag || tag;
+        pkg.publishConfig.tag = tag;
+
+        return fs.writeJson(pkgPath, pkg, { spaces: 2 });
       }
-      return fs.writeJson(pkgPath, pkg, { spaces: 2 });
     })
     .then(() => tag);
 };
